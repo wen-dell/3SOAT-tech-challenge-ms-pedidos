1d4e852e6786cc264d0e3884308fc88cace4cfb2
Wendell Alves <wendellmccaulay@hotmail.com>
2024-05-18 20:27:10 -0300

    Classes desnecessárias

--- src/main/java/br/com/tech/challenge/api/exception/ClienteAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package br.com.tech.challenge.api.exception;
-
-public class ClienteAlreadyExistsException extends RuntimeException {
-
-    public ClienteAlreadyExistsException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/br/com/tech/challenge/api/exception/MercadoPagoAPIException.java
+++
@@ -1,9 +0,0 @@
-package br.com.tech.challenge.api.exception;
-
-public class MercadoPagoAPIException extends RuntimeException {
-
-    public MercadoPagoAPIException(String msg) {
-        super(msg);
-    }
-
-}
